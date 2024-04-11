fc7f7386a35a8735bb7c0004c49a6838eca35a2d
Magdalena Jarošová <amnerabb@gmail.com>
2024-04-11 14:31:36 +0200

    note validation

--- film.js
+++ film.js
@@ -125,8 +125,8 @@ addNote.addEventListener('click', (event) => {
   if (noteValue.value.lenght === 0){
 	noteValue.classList.add("is-invalid")
   } else {
-	const noteValid = document.querySelector('#form-check-label')
-	if(event.code !== "autocompleted"){
+	const noteValid = document.querySelector('.form-check-label');
+	if(noteValid.checked){
 		noteValue.classList.add("is-invalid")
 	}else{
 		noteValue.innerHTML = `<p class="card-text">${noteValue.value}</p>`
